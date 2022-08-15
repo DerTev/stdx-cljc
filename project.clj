bb2a1e05939734c5d99292e2e0b21495c6fe7aba
Tim <77175275+DerTev@users.noreply.github.com>
2022-08-15 23:58:28 +0200

    Add Group ID

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject stdx-cljc "0.1.0"
+(defproject de.dertev/stdx-cljc "0.1.0"
   :description "Standart Library Extensions for Clojure and ClojureScript"
   :url "https://github.com/dertev/stdx-cljc"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot stdx-cljc.core
   :repositories [["devcord" {:url "https://repo.devcord.club/"
-                              :username [:gpg :env/devcord_username]
-                              :password [:gpg :env/devcord_password]}]])
+                              :username :env/devcord_username
+                              :password :env/devcord_password}]])
