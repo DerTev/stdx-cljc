8f938e8b94b0de3100955f3b1bee0afec16aa120
Tim <77175275+DerTev@users.noreply.github.com>
2022-08-16 00:25:19 +0200

    Remove main

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/dertev/stdx-cljc"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :main ^:skip-aot stdx-cljc.core
-  :repositories [["devcord" {:url "https://repo.devcord.club/releases"
+  :repositories [["releases" {:url "https://repo.devcord.club/releases"
                               :username :env/devcord_username
                               :password :env/devcord_password}]])
